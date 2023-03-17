cd9e8f8f2d83f7cb11cbb7abd7a4364fdfe56474
« Matteo <matteo.courquin@gmail.com>
2023-03-17 11:48:58 +0100

    page about

--- components/About.vue
+++ components/About.vue
@@ -1,30 +1,32 @@
 <template>
   <div class="about">
-
     <div class="wrapper-about-titles">
       <div class="about-year about-year_1">
-        <span>20</span>
+        <span class="anim-about-year-1">20</span>
       </div>
       <div class="titles-about">
-        <h2>GOLDEN</h2>
-        <h2>COLLECTION</h2>
+        <div class="image-about"></div>
+        <h2 class="anim-title-about-1">GOLDEN</h2>
+        <h2 class="anim-title-about-2">COLLECTION</h2>
       </div>
       <div class="about-year about-year_2">
-        <span>22</span>
+        <span class="anim-about-year-2">22</span>
       </div>
     </div>
 
     <div class="about-footer">
       <div class="tile-about">
-        <h3>SOAP STORE</h3>
+        <h3 class="anim-title-about-3">SOAP STORE</h3>
       </div>
-      <div class="line-vertical-footer"></div>
-      <NuxtLink to="/shop" class="links-array text-anim text-anim-2">Shop Now<svg id="Calque_1" data-name="Calque 1"
-          xmlns="http://www.w3.org/2000/svg" viewBox="0 0 277.24 546.58" width="1rem" height="1rem">
-          <polyline class="cls-1" fill="transparent" stroke="#000" stroke-width="40"
-            points="2.48 2.47 272.3 273.29 2.48 544.11" />
-        </svg></NuxtLink>
-      <div class="line-vertical-footer"></div>
+      <div class="line-vertical-footer line-vertical-footer_1"></div>
+      <div class="link-about">
+        <NuxtLink to="/shop" class="links-array anim-title-about-4">Shop Now<svg id="Calque_1" data-name="Calque 1"
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 277.24 546.58" width="1rem" height="1rem">
+            <polyline class="cls-1" fill="transparent" stroke="#000" stroke-width="40"
+              points="2.48 2.47 272.3 273.29 2.48 544.11" />
+          </svg></NuxtLink>
+      </div>
+      <div class="line-vertical-footer line-vertical-footer_2"></div>
       <div class="about-star">
         <div class='anim-about-star'>
           <img class="anim-rotate" src="~/assets/images/icons/star-8.svg" alt="">
@@ -50,10 +52,12 @@ export default {
 
 .about {
   height: 100vh;
+  width: 100%;
   padding-top: 80px;
   display: grid;
   grid-template-rows: 1fr 150px;
   position: relative;
+  overflow: hidden;
 
   .line-vertical-page {
     background: $color-black;
@@ -61,10 +65,12 @@ export default {
 
   .wrapper-about-titles {
     height: 100%;
+    width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     text-align: center;
+    overflow: hidden;
   }
 
   .about-year,
@@ -74,6 +80,18 @@ export default {
     aspect-ratio: 1/1;
     height: 60%;
     position: absolute;
+
+    .anim-title-about-1 {
+      position: absolute;
+      top: 0;
+      transform: translateY(-50vh);
+    }
+
+    .anim-title-about-2 {
+      position: absolute;
+      bottom: 0;
+      transform: translateY(50vh);
+    }
   }
 
   .about-year {
@@ -81,27 +99,52 @@ export default {
     justify-content: center;
     align-items: center;
 
+    @media screen and (max-width : 1000px) {
+      display: none;
+    }
+
     span {
       font-family: $font-new-york;
       color: $color-black;
       font-weight: normal;
       font-size: clamp(2rem, 5vw, 6rem);
+      opacity: 0;
+
+      @media screen and (max-width : 1300px) {
+        display: none;
+      }
+    }
+
+    .anim-about-year-1 {
+      transform: translateX(20vh);
+    }
+
+    .anim-about-year-2 {
+      transform: translateX(-20vh);
     }
   }
 
   .about-year_1 {
-    left: 0;
+    left: -100vw;
   }
 
   .about-year_2 {
-    right: 0;
+    right: -100vw;
   }
 
   .titles-about {
-    background-image: url('assets/images/about/illustration1.jpg');
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
+    .image-about {
+      background-image: url('assets/images/about/illustration1.jpg');
+      background-position: center;
+      background-size: cover;
+      background-repeat: no-repeat;
+      overflow: hidden;
+      border-radius: 9999px;
+      width: 100%;
+      height: 100%;
+      transform: translateY(100vh);
+    }
+
     border: none;
     display: flex;
     justify-content: space-between;
@@ -118,15 +161,35 @@ export default {
   .about-footer {
     position: relative;
     display: flex;
-    align-items: center;
     justify-content: flex-end;
+    z-index: 1;
+    background: $color-white;
 
-    .tile-about{
+    .tile-about {
       width: 100%;
       padding: 0 80px;
+      display: flex;
+      align-items: center;
+
+      .anim-title-about-3 {
+        transform: translateX(-700px);
+      }
+
       h3 {
         font-size: clamp(1rem, 5vw, 2rem);
       }
+    }
+
+    .anim-title-about-4 {
+      transform: translateX(-700px);
+    }
+
+    .link-about {
+      width: fit-content;
+      overflow: hidden;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
 
     .links-array {
@@ -139,12 +202,13 @@ export default {
       left: 0;
       background: $color-black;
       height: 1px;
-      width: 100%;
+      width: 0%;
     }
 
     .line-vertical-footer {
       background: $color-black;
-      height: 100%;
+      height: 0%;
+      bottom: 0;
       width: 1px;
     }
 
--- pages/about.vue
+++ pages/about.vue
@@ -34,8 +34,18 @@ export default {
         .to('.header-line', { width: '100vw', duration: 4, ease: 'power4.out' })
         .to('.line-vertical-page_left', { height: '100vh', duration: 4, ease: 'power4.out' }, '-=4')
         .to('.line-vertical-page_right', { height: '100vh', duration: 4, ease: 'power4.out' }, '-=4')
-        // .from('.titles-about', {y: '100vw', duration: 4, ease: 'power4.out' }, '-=4')
-
+        .to('.line-horizontal-footer', { width: '100%', duration: 4, ease: 'power4.out' }, '-=4')
+        .to('.line-vertical-footer_1', { height: '100%', duration: 4, ease: 'power4.out' }, '-=3.3')
+        .to('.line-vertical-footer_2', { height: '100%', duration: 4, ease: 'power4.out' }, '-=3.4')
+        .to('.anim-title-about-1', {y: '0', duration: 2, ease: 'power4.out' }, '-=4')
+        .to('.anim-title-about-2', {y: '0', duration: 2, ease: 'power4.out' }, '-=4')
+        .to('.image-about', {y: '0', duration: 2, ease: 'power4.out' }, '-=4')
+        .to('.anim-about-year-1', {x: '0', opacity: '1', duration: 2, ease: 'power4.out' }, '-=2')
+        .to('.anim-about-year-2', {x: '0', opacity: '1', duration: 2, ease: 'power4.out' }, '-=2')
+        .to('.about-year_1', {left: '0', opacity: '1', duration: 3, ease: 'power4.out' }, '-=3.4')
+        .to('.about-year_2', {right: '0', opacity: '1', duration: 3, ease: 'power4.out' }, '-=3.4')
+        .to('.anim-title-about-3', {x: '0', opacity: '1', duration: 3, ease: 'power4.out' }, '-=3.4')
+        .to('.anim-title-about-4', {x: '0', opacity: '1', duration: 3, ease: 'power4.out' }, '-=3.4')
       tl.play();
     }
   },
